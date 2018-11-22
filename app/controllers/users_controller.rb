9c75890a8270f36a26b6ca11fd570e1599a2205c
King_Rocks <rishi12398@gmail.com>
2018-11-23 03:22:49 +0530

    Progress Tracker Corrected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
   end
   def flop
     @user = current_user
-    if ((Time.now - @user.updated_at)/3600 > 12)
-      @user.progress+=20
-      @user.days+=1
+    if (@user.days)
+      if ((Time.now - @user.updated_at)/3600 > 12)
+        @user.progress+=20
+        @user.days+=1
+        @user.save
+      end
+    else
+      @user.progress=20
+      @user.days=1
       @user.save
     end
     redirect_to schedules_path
